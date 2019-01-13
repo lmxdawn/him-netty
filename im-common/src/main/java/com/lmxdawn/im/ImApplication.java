d7b30cce1bcb3682043b8d16bfd4c6d5a7bb458c
lmxdawn <lmxdawn@qq.com>
2019-01-13 19:51:37 +0800

    第一次提交

---
+++ im-common/src/main/java/com/lmxdawn/im/ImApplication.java
@@ -0,0 +1,14 @@
+package com.lmxdawn.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImApplication.class, args);
+    }
+
+}
+
